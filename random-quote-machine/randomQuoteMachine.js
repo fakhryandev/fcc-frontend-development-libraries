5494985fc7ec7a85c8ef32f7b67a2895e3c6573f
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-05-18 22:37:24 +0700

    styling random quote machine

--- random-quote-machine/randomQuoteMachine.js
+++ random-quote-machine/randomQuoteMachine.js
@@ -1,9 +1,24 @@
 $(document).ready(() => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   getRandomQuote();
 });
 
 $("#new-quote").click(() => {
+  document.getElementById("text").style.opacity = "0";
+  document.getElementById("author").style.opacity = "0";
+
   getRandomQuote();
+
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.transition = "all 0.5s ease";
 });
 
 const getRandomQuote = () => {
@@ -18,6 +33,9 @@ const getRandomQuote = () => {
 };
 
 const renderToPage = ({ content, author }) => {
-  $("#text").text(content);
-  $("#author").text(author);
+  document.getElementById("text").style.opacity = "1";
+  document.getElementById("author").style.opacity = "1";
+
+  $("#text").text(`" ${content}`);
+  $("#author").text(`-- ${author}`);
 };
