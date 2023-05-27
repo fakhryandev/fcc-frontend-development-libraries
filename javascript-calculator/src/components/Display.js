241d4f68ab2b4c723e74b6ecc6fca29092ad20fd
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-05-27 23:40:54 +0700

    downgrade to react 17

--- javascript-calculator/src/components/Display.js
+++ javascript-calculator/src/components/Display.js
@@ -1,7 +1,9 @@
 const Display = ({ input, output }) => (
   <>
     <div className="text-sm text-right text-yellow-400 h-5">{output}</div>
-    <div className="text-right text-3xl h-10">{input}</div>
+    <div id="display" className="text-right text-3xl h-10">
+      {input}
+    </div>
   </>
 );
 
